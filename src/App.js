65e150322dbb0a543747731a66b6f63cb99236cc
Meianna Oeser <meianna.oeser@gmail.com>
2020-08-26 17:50:48 -0700

    updated resume

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import './App.css';
 import Nav from "./components/Nav";
-import Footer from "./components/Footer";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio.js";
-import Contact from "./components/Contact";
 import Header from "./components/Header";
+import Portfolio from "./components/Portfolio.js";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
